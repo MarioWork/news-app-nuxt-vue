94181b3eabf4f85d4ab67fb38c7a895e71fef472
MarioWork <mariovieira.work@hotmail.com>
2023-04-05 21:24:09 +0000

    new layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,46 @@
 <template>
-  <v-card>
-    <v-toolbar>
-      <v-toolbar-title
-        ><h1 class="text-center py-6">{{ currentCategory }} News</h1>
+  <v-container fluid style="padding: 0">
+    <v-toolbar class="card">
+      <v-toolbar-title style="color: black" class="text-center">
+        <h1 class="py-6">{{ currentCategory.title }} News</h1>
       </v-toolbar-title>
+
       <template #extension>
-        <v-tabs v-model="currentCategory" align-tabs="center" grow>
-          <v-tab value="All"> All </v-tab>
-          <v-tab value="Tech"> Tech </v-tab>
+        <v-tabs
+          v-model="currentCategory"
+          bg-color="transparent"
+          grow
+          fixed-tabs
+        >
+          <v-tab
+            v-for="category in categories"
+            :key="category.id"
+            :value="category"
+            style="color: black"
+          >
+            {{ category.title }}
+          </v-tab>
         </v-tabs>
       </template>
     </v-toolbar>
-  </v-card>
+  </v-container>
 </template>
 
 <script setup>
-const currentCategory = ref("All");
+const categories = [
+  { id: 1, title: "All" },
+  { id: 2, title: "Tech" },
+];
+const currentCategory = ref(categories[0]);
 </script>
+
+<style scope>
+* {
+  color: white;
+  font-weight: bold;
+}
+
+.card {
+  background-color: #219ebc;
+}
+</style>
