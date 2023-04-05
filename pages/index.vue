a93392bc3edd6ae624f15b6f1420714ffda3ee65
MarioWork <mariovieira.work@hotmail.com>
2023-04-05 20:02:36 +0000

    First iteration toolbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <v-btn>Hello World</v-btn>
-  </div>
+  <v-layout>
+    <v-app-bar color="grey">
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>{{ currentCategory }} News</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <v-list>
+        <v-list-item title="All"></v-list-item>
+        <v-list-item title="Technology"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-layout>
 </template>
 
-<script setup></script>
+<script setup>
+const drawer = ref(false);
+const currentCategory = ref("All");
+</script>
