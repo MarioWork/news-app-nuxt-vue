d14618e3694c2090d847efe638eb095951c2cda2
MarioWork <mariovieira.work@hotmail.com>
2023-04-07 21:35:25 +0000

    first design iteration for article card

---
+++ components/article-card.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ article.title }}</div>
+</template>
+
+<script setup>
+const props = defineProps({
+  article: { type: Object, required: true },
+});
+
+const { article } = toRefs(props);
+</script>
+
+<style scoped></style>
