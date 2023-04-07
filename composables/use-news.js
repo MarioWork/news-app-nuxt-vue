6a415e54254e6fdf4b485fa14ffb48b4d6b7d754
MarioWork <mariovieira.work@hotmail.com>
2023-04-07 00:21:17 +0000

    first iteration of composable

--- composables/use-news.js
+++ composables/use-news.js
@@ -1,18 +1,38 @@
-export function useNews() {
-  const categories = [
-    { id: 1, title: "General", value: "general" },
-    { id: 2, title: "Technology", value: "technology" },
-    { id: 3, title: "Business", value: "business" },
-    { id: 3, title: "Entertainment", value: "entertainment" },
-    { id: 3, title: "Science", value: "science" },
-    { id: 3, title: "Health", value: "health" },
-    { id: 4, title: "Sports", value: "sports" },
-  ];
+const categories = [
+  { id: 1, title: "General", value: "general" },
+  { id: 2, title: "Technology", value: "technology" },
+  { id: 3, title: "Business", value: "business" },
+  { id: 3, title: "Entertainment", value: "entertainment" },
+  { id: 3, title: "Science", value: "science" },
+  { id: 3, title: "Health", value: "health" },
+  { id: 4, title: "Sports", value: "sports" },
+];
 
-  const currentCategory = ref(categories[0]);
+export async function useNews() {
+  const config = useRuntimeConfig();
+
+  const currentCategory = ref(categories[1]);
+
+  const {
+    data: news,
+    pending,
+    refresh,
+  } = await useLazyFetch(() => `/top-headlines`, {
+    baseURL: config.baseURL,
+    query: {
+      apiKey: config.newsApiKey,
+      category: currentCategory.value.value,
+    },
+  });
+
+  watch(currentCategory, async () => {
+    await refresh();
+  });
 
   return {
     currentCategory,
     categories,
+    pending,
+    news,
   };
 }
