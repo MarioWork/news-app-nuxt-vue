39be7b714f2d279e1e998bae264e8b442761e34a
MarioWork <mariovieira.work@hotmail.com>
2023-04-08 02:45:36 +0000

    remove unecessary div

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,13 @@
       @category-selected="categorySelected"
     />
     <div v-if="pending">Loading...</div>
-    <div v-else id="test">
-      <ArticleCard
-        v-for="article in news.articles"
-        :key="article.id"
-        :article="article"
-      >
-      </ArticleCard>
-    </div>
+    <ArticleCard
+      v-for="article in news.articles"
+      v-else
+      :key="article.id"
+      :article="article"
+    >
+    </ArticleCard>
   </v-card>
 </template>
 
@@ -32,12 +31,5 @@ const categorySelected = (selected) => {
 
 .noBorderRadius {
   border-radius: 0;
-}
-
-#test {
-  padding: 1em;
-  display: flex;
-  flex-direction: column;
-  gap: 1em;
 }
 </style>
