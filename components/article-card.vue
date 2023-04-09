9e4f535309eb1844553aa68ac6ebdbc5ab558071
MarioWork <mariovieira.work@hotmail.com>
2023-04-09 01:48:30 +0000

    fix design

--- components/article-card.vue
+++ components/article-card.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-card>
-    <v-img :src="article.urlToImage" cover></v-img>
-    <v-card-text>
+  <v-card class="pa-6 elevation-4 h-100">
+    <v-img :src="article.urlToImage" cover height="400"></v-img>
+    <v-card-text class="text-h6">
       {{ article.title }}
     </v-card-text>
-    <v-card-subtitle>
+    <v-card-text>
       {{ article.description }}
-    </v-card-subtitle>
+    </v-card-text>
   </v-card>
 </template>
 
-<script setup>
+<script allowJs setup>
 const props = defineProps({
   article: { type: Object, required: true },
 });
