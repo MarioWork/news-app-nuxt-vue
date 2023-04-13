47f3421d3285d599e4aefee4b04a2f45fd01ef0e
MarioWork <mariovieira.work@hotmail.com>
2023-04-13 23:33:04 +0000

    add a default image when there is no image link provided for the article

--- components/article-card.vue
+++ components/article-card.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="pa-6 elevation-4 h-100">
-    <v-img :src="article.urlToImage" cover height="400"></v-img>
+    <v-img :src="articleThumbnail" cover height="400"></v-img>
     <v-card-text class="text-h6 font-weight-bold">
       {{ article.title }}
     </v-card-text>
@@ -37,6 +37,12 @@ const publishedAtString = computed(() => {
     (publishedAtDate.getUTCMonth() + 1).toString().padStart(2, "0"),
     publishedAtDate.getUTCFullYear(),
   ].join("/");
+});
+
+const articleThumbnail = computed(() => {
+  return article.value.urlToImage
+    ? article.value.urlToImage
+    : "https://img.freepik.com/free-photo/top-view-old-french-newspaper-pieces_23-2149318857.jpg?w=2000&t=st=1681428463~exp=1681429063~hmac=4079efc95881044810b96f8a70a191fbfa2d648d4295f75461fd67e71d804b8f";
 });
 </script>
 
