a079f356e998f5236bfe4784e1fcb8456880a8b5
MarioWork <mariovieira.work@hotmail.com>
2023-04-14 17:31:07 +0000

    loadmore on click

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
     <v-main v-else scrollable="true" class="h-100">
       <v-row class="pa-6">
         <v-col
-          v-for="article in news.articles"
+          v-for="article in articles"
           :key="article.id"
           cols="12"
           lg="6"
@@ -18,16 +18,25 @@
           <ArticleCard :article="article"> </ArticleCard>
         </v-col>
       </v-row>
+      <v-btn @click="nextPage">Press me</v-btn>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-const { categories, currentCategory, news, pending } = useNews();
+const { categories, currentCategory, news, pending, nextPage } = useNews();
+const articles = ref();
 
 const categorySelected = (selected) => {
   currentCategory.value = selected;
+  articles.value = undefined;
 };
+
+watch(news, (newValue) => {
+  articles.value = articles.value
+    ? [...articles.value, ...newValue.articles]
+    : newValue.articles;
+});
 </script>
 
 <style scope>
