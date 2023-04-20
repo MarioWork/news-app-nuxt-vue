a984f28b4ef21ecac4162c8dee1b6e7ee715c86e
MarioWork <mariovieira.work@hotmail.com>
2023-04-20 21:44:50 +0000

    started design of details page

--- pages/article/details/index.vue
+++ pages/article/details/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>{{ state }}</div>
+  <v-container class="h-screen">
+    <v-row class="h-25"> </v-row>
+    <v-row class="h-75">
+      <v-col cols="12">
+        <div>{{ state }}</div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
