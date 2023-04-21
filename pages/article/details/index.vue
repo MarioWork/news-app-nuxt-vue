175e15b51f9589c8a7bdfb7f88c8252a465eda8a
MarioWork <mariovieira.work@hotmail.com>
2023-04-21 19:46:54 +0000

    continued design of details page

--- pages/article/details/index.vue
+++ pages/article/details/index.vue
@@ -1,16 +1,35 @@
 <template>
-  <v-container class="h-screen">
-    <v-row class="h-25"> </v-row>
-    <v-row class="h-75">
-      <v-col cols="12">
-        <div>{{ state }}</div>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-card class="remove-padding border-none">
+    <v-container class="w-100 h-screen remove-padding">
+      <v-row class="h-50">
+        <v-img :src="articleThumbnail" class="w-100" aspect-ratio="16/9" cover>
+        </v-img>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <div>{{ state }}</div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script setup>
 const state = useState("article");
+
+const articleThumbnail = computed(() => {
+  return state.value.urlToImage
+    ? state.value.urlToImage
+    : "https://img.freepik.com/free-photo/top-view-old-french-newspaper-pieces_23-2149318857.jpg?w=2000&t=st=1681428463~exp=1681429063~hmac=4079efc95881044810b96f8a70a191fbfa2d648d4295f75461fd67e71d804b8f";
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.remove-padding {
+  padding: 0;
+}
+
+.border-none {
+  border-radius: 0;
+}
+</style>
