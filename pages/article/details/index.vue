329569097f5160bf1894d3f8d093f30d76adf9bd
MarioWork <mariovieira.work@hotmail.com>
2023-04-23 18:58:00 +0000

    get article from local storage

--- pages/article/details/index.vue
+++ pages/article/details/index.vue
@@ -8,17 +8,17 @@
       <v-row>
         <v-col cols="12">
           <v-card-text class="text-h6 font-weight-bold">
-            {{ state.title }}
+            {{ article.title }}
           </v-card-text>
           <v-card-text>
-            {{ state.description }}
+            {{ article.description }}
           </v-card-text>
           <v-card-text>
-            {{ state?.content }}
+            {{ article?.content }}
           </v-card-text>
         </v-col>
       </v-row>
-      <nuxt-link :to="state.url">
+      <nuxt-link :to="article.url">
         <v-btn>Go to Article</v-btn>
       </nuxt-link>
     </v-container>
@@ -26,11 +26,12 @@
 </template>
 
 <script setup>
-const state = useState("article");
+import nuxtStorage from "nuxt-storage";
 
+const article = ref(JSON.parse(nuxtStorage.localStorage.getData("test")));
 const articleThumbnail = computed(() => {
-  return state.value.urlToImage
-    ? state.value.urlToImage
+  return article.value.urlToImage
+    ? article.value.urlToImage
     : "https://img.freepik.com/free-photo/top-view-old-french-newspaper-pieces_23-2149318857.jpg?w=2000&t=st=1681428463~exp=1681429063~hmac=4079efc95881044810b96f8a70a191fbfa2d648d4295f75461fd67e71d804b8f";
 });
 </script>
